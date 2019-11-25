88354d059d52a1204923ef713a059d3ef5692b76
opzyra <opzyra@gmail.com>
2019-11-25 21:49:02 +0900

    Dev backend test code

---
+++ src/test/pagination.spec.js
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { pagination } from "../lib/pagination";
+
+describe("lib/pagination 테스트", () => {
+  describe("pagination - 페이징 계산 처리", () => {
+    it("페이지수가 남는 경우", () => {
+      const sample = {
+        rowCount: 15,
+        limit: 10,
+        page: 2
+      };
+      const result = pagination(sample.rowCount, sample.limit, sample.page);
+      const expected = {
+        rowCount: 15,
+        totalPages: 2,
+        page: 2
+      };
+      expect(result).to.eql(expected);
+    });
+
+    it("페이지수가 맞아 떨어지는 경우", () => {
+      const sample = {
+        rowCount: 20,
+        limit: 10,
+        page: 1
+      };
+      const result = pagination(sample.rowCount, sample.limit, sample.page);
+      const expected = {
+        rowCount: 20,
+        totalPages: 2,
+        page: 1
+      };
+      expect(result).to.eql(expected);
+    });
+  });
+});

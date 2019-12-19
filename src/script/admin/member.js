2c0308dffd699bdcac9f1038ada69e17f299864d
opzyra <opzyra@gmail.com>
2019-12-19 15:59:27 +0900

    Dev admin member email

--- src/script/admin/member.js
+++ src/script/admin/member.js
@@ -2,12 +2,19 @@ import { COMMON, BROWSER, AJAX } from "../common";
 
 export const member = {
   namespace: "member",
-
+  toolTip() {
+    tippy(".member .lib .right .list ul li .contents .name .email", {
+      placement: "bottom",
+      theme: "devhyun",
+      distance: 2
+    });
+  },
   init() {
     COMMON.pagination(
       "#page",
       STATE.memberPage.totalPages,
       STATE.memberPage.page
     );
+    this.toolTip();
   }
 };

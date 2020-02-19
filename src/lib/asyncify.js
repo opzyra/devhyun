1442122ac1de61cc653cba472f91473910cc9594
opzyra <opzyra@gmail.com>
2020-02-19 21:08:03 +0900

    Fix asyncify express

--- src/lib/asyncify.js
+++ src/lib/asyncify.js
@@ -1,5 +1,46 @@
 import express from 'express';
-import asyncify from 'express-asyncify';
+import database from '@/models';
+
+const httpVerbsArray = ['get', 'post', 'put', 'delete', 'patch'];
+
+const asyncify = app => {
+  return new Proxy(app, {
+    get(app, prop) {
+      if (httpVerbsArray.includes(prop)) {
+        return delegate(app, prop);
+      } else {
+        return app[prop] && app[prop].bind && app[prop].bind(app);
+      }
+    },
+  });
+};
+
+const delegate = (app, prop) => {
+  const func = app[prop].bind(app);
+  return (url, ...funcs) => {
+    return func(
+      url,
+      ...funcs.map(e =>
+        e[Symbol.toStringTag] === 'AsyncFunction' ? wrap(e) : e,
+      ),
+    );
+  };
+};
+
+const wrap = asyncFunc => {
+  return async (req, res, next) => {
+    const transaction = await database.sequelize.transaction();
+    req.transaction = transaction;
+    asyncFunc(req, res, next)
+      .then(async () => {
+        await transaction.commit();
+      })
+      .catch(async error => {
+        await transaction.rollback();
+        next(error);
+      });
+  };
+};
 
 export default function router() {
   return asyncify(express.Router());

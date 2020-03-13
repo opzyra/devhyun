f1d5067ceee96207205653d816ad8ec547b7c216
opzyra <opzyra@gmail.com>
2020-03-13 16:44:46 +0900

    Fix api router eslint

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,27 +1,27 @@
-import express from "express";
+import express from 'express';
 
-import upload from "./upload";
-import editor from "./editor";
-import member from "./member";
-import group from "./group";
-import schedule from "./schedule";
-import task from "./task";
-import note from "./note";
-import post from "./post";
-import series from "./series";
-import comment from "./comment";
+import upload from './upload';
+import editor from './editor';
+import member from './member';
+import group from './group';
+import schedule from './schedule';
+import task from './task';
+import note from './note';
+import post from './post';
+import series from './series';
+import comment from './comment';
 
 const router = express.Router();
 
-router.use("/upload", upload);
-router.use("/editor", editor);
-router.use("/member", member);
-router.use("/group", group);
-router.use("/schedule", schedule);
-router.use("/task", task);
-router.use("/note", note);
-router.use("/post", post);
-router.use("/series", series);
-router.use("/comment", comment);
+router.use('/upload', upload);
+router.use('/editor', editor);
+router.use('/member', member);
+router.use('/group', group);
+router.use('/schedule', schedule);
+router.use('/task', task);
+router.use('/note', note);
+router.use('/post', post);
+router.use('/series', series);
+router.use('/comment', comment);
 
 export default router;
